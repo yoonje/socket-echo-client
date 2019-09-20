e6450f958373998ae22288f795b32807ed526589
yoonje <elxm6123@gmail.com>
2019-09-20 23:17:54 +0900

    remove buffer

--- main.c
+++ main.c
@@ -5,18 +5,14 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-#define  BUFF_SIZE   1024
-
-int main(int argc, char **argv) {
+void
+main(int argc, char **argv) {
     int client_socket;
 
     struct sockaddr_in server_addr;
 
-    char buff[BUFF_SIZE + 5];
-
-    client_socket = socket(PF_INET, SOCK_STREAM, 0); // PF_INET은 프로토콜 체계에서 IPv4, SOCK_STREAM은 TCP를 의미
-    if (-1 == client_socket) {
-        printf("socket 생성 실패\n");
+    if (argv[1] == NULL) {
+        printf("메세지 입력 필요\n");
         exit(1);
     }
 
@@ -25,11 +21,18 @@ int main(int argc, char **argv) {
     server_addr.sin_port = htons(4000); // htons 함수를 통해서 Little Endian일 경우 Big Endian으로 변경하여 포트 번호 대입
     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // 32bit IPv4 주소로 초기화
 
+    client_socket = socket(PF_INET, SOCK_STREAM, 0); // PF_INET은 프로토콜 체계에서 IPv4, SOCK_STREAM은 TCP를 의미
+
+    if (-1 == client_socket) {
+        printf("socket 생성 실패\n");
+        exit(1);
+    }
+
     if (-1 == connect(client_socket, (struct sockaddr *) &server_addr, sizeof(server_addr))) { // 연결 요청
         printf("접속 실패\n");
         exit(1);
     }
-    write(client_socket, argv[1], strlen(argv[1]) + 1); // NULL까지 포함해서 전송하기 위해 +1
+
+    write(client_socket, argv[1], strlen(argv[1]) + 1); // 메세지 write, NULL까지 포함해서 전송하기 위해 +1
     close(client_socket); // 소켓 닫기
-    return 0;
 }
